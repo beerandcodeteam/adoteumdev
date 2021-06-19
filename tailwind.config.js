4dec26d334bbdebd917c986e02c87cb2179d8fb6
Lucas Souza <lucasrsouzasilva@gmail.com>
2021-06-19 11:42:37 -0300

    configuração do tailwind e gostinho inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: {
+                    100: '#FE3C72',
+                    75: '#FE6B94',
+                    50: '#FE9DB8',
+                    25: '#FFCEDC'
+                },
+                secondary: {
+                    100: '#FF7854',
+                    75: '#FF9A7F',
+                    50: '#FFBBA9',
+                    25: '#FFDDD4'
+                },
+                grey: {
+                    100: '#424242',
+                    75: '#717171',
+                    50: '#A0A0A0',
+                    25: '#D0D0D0'
+                }
+            }
         },
     },
     variants: {
