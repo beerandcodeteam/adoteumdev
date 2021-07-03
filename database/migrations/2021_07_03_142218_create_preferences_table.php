fadab64e527c158273b3a45a4a7baa36f537949e
Icaro William <icarojobsoficial@gmail.com>
2021-07-03 11:36:53 -0300

    feat: adicionado tabela skills para adicionar a tecnologia/conhecimento que o desenvolvedor possui

---
+++ database/migrations/2021_07_03_142218_create_preferences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preferences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->json('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preferences');
+    }
+}
