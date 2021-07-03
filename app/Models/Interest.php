bbafd467566e84246b624a5c9736ec144a153a4a
Icaro William <icarojobsoficial@gmail.com>
2021-07-03 11:20:46 -0300

    feat: criado tabela interests + model

---
+++ app/Models/Interest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Interest extends Model
+{
+    use HasFactory;
+
+    protected $casts = ['data' => 'array'];
+}
