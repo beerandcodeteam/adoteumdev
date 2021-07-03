f974e7df3b6dab7577c70b639d70fc71855ae9da
Icaro William <icarojobsoficial@gmail.com>
2021-07-03 12:05:14 -0300

    feat: adicionado tabela actions para controle de likes, dislikes e superlikes

---
+++ database/migrations/2021_07_03_145647_create_actions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('from_user_id')->constrained('users');
+            $table->foreignId('to_user_id')->constrained('users');
+            $table->enum('name', ['LIKE', 'DISLIKE', 'SUPERLIKE']);
+            $table->date('expiration_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actions');
+    }
+}
