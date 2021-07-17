08c44c9ebbe5a6a125fa0a647862e355293603d5
Icaro William <icarojobsoficial@gmail.com>
2021-07-17 11:05:25 -0300

    feat: added route to home screen

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,13 @@ use App\Http\Livewire\Auth\Passwords\Email;
 use App\Http\Livewire\Auth\Passwords\Reset;
 use App\Http\Livewire\Auth\Register;
 use App\Http\Livewire\Auth\Verify;
+use App\Http\Livewire\Components\HomeScreen;
 use App\Http\Livewire\Components\SplashScreen;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', SplashScreen::class)->name('home');
+Route::get('/', SplashScreen::class)->name('app.splash');
+Route::get('home', HomeScreen::class)->name('app.home');
 
 Route::middleware('guest')->group(function () {
     Route::get('login', Login::class)
