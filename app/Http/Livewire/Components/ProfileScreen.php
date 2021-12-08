28e729d83ad9d30d894b69a837d86dadd5aac719
Kelver <kelver_kmv@hotmail.com>
2021-12-08 17:15:44 -0300

    Feat: Melhoria no Upload de avatar.

--- app/Http/Livewire/Components/ProfileScreen.php
+++ app/Http/Livewire/Components/ProfileScreen.php
@@ -4,7 +4,11 @@ namespace App\Http\Livewire\Components;
 
 use App\Models\Profile;
 use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
+use JetBrains\PhpStorm\NoReturn;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -20,20 +24,33 @@ class ProfileScreen extends Component
         $this->validate([
             'imageProfile' => 'image',
         ]);
+        $this->storeAvatar();
+    }
+
+    public function storeAvatar(): bool
+    {
+//        TODO: validar local de upload.
+//        TODO: Cada vez que faz login, atualiza a imagem com a do github.
         $path = $this->imageProfile->store('avatarProfile', 'public');
         $profile = Profile::find($this->loggedUser['profile']['id']);
         $profile->avatar = url("storage/{$path}");
-        $profile->save();
-        $this->imageProfile = $profile->avatar;
+
+        if ($profile->save()) {
+            $this->imageProfile = null;
+            $this->loggedUser['profile']['avatar'] = $profile->avatar;
+            return true;
+        }
+
+        return false;
     }
 
-    public function mount(User $user)
+    public function mount(User $user): void
     {
         $this->loggedUser = User::with('profile')
             ->find(Auth::user()->id)?->toArray();
     }
 
-    public function render()
+    public function render(): Factory|View|Application
     {
         return view('livewire.components.profile-screen');
     }
--- resources/views/livewire/components/profile-screen.blade.php
+++ resources/views/livewire/components/profile-screen.blade.php
@@ -1,8 +1,9 @@
 <div
     class="flex flex-col min-h-screen justify-between w-full items-center bg-gray-10 overflow-hidden"
-    x-data="chat({
-        loggedUser: @entangle('loggedUser')
-    })"
+    x-data="{
+        loggedUser: @entangle('loggedUser'),
+        files: null
+    }"
 >
     <div class="flex flex-row w-full px-7 py-4 justify-between items-center fixed bg-gray-10 z-50 shadow">
         <a href="{{ route('app.developers') }}"  class="cursor-pointer transform duration-150 active:scale-95">
@@ -19,26 +20,40 @@
         <div class="md:col-span-1">
             <div class="flex flex-col items-center justify-center space-y-2">
                 @if($this->imageProfile)
-                    <img class="inline-block h-24 w-24 rounded-full" src="{{ $this->imageProfile->temporaryUrl() }}" alt="" />
+                    <img class="inline-block h-24 w-24 rounded-full" src="{{ $this->imageProfile->temporaryUrl() }}"
+                         alt="" />
                 @else
                     <img class="inline-block h-24 w-24 rounded-full" src="{{ $loggedUser['profile']['avatar'] }}" alt="" />
                 @endif
-                <div class="flex flex-row w-1/3">
-                    <label
-                        class="w-full flex flex-row items-center justify-center px-2 h-10
-                            bg-white rounded-md shadow-md tracking-wide uppercase
-                            border border-blue cursor-pointer hover:bg-primary-100
-                            hover:text-white text-primary-100 ease-linear transition-all
-                            space-x-4 duration-150"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 py-1" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
-                        </svg>
-                        <span class="text-xs items-center">Alterar avatar</span>
-                        <input wire:model="imageProfile" wire:change="uploadAvatar" type="file"
-                               class="hidden" name="imageProfile" />
-                    </label>
+                <div class="flex flex-row w-1/3 items-center justify-center">
+                    <form name="formAvatar" action="" method="post" wire:submit.prevent="uploadAvatar">
+                        <label
+                            class="w-full min-w-full block flex flex-row items-center justify-center px-2 h-10
+                            bg-white rounded-md shadow-md uppercase border border-primary-100
+                            cursor-pointer hover:bg-primary-100 hover:text-white text-primary-100
+                            ease-linear transition-all space-x-4 duration-150"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 py-1" fill="none" viewBox="0 0 24 24"
+                                 stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                            </svg>
+                            <input wire:model="imageProfile" type="file" class="hidden" name="imageProfile"
+                                   x-on:change="files = Object.values($event.target.files)" accept="image/jpg,image/jpeg,image/png"
+                            />
+                            <span
+                                x-text="files
+                                            ? files.map(file => file.name).join(', ')
+                                            : 'Selecione o Avatar'"
+                                  class="w-full max-w-full text-xs items-center"></span>
+                        </label>
+                        <div class="w-full flex flex-row justify-end">
+                            <button type="submit" @click="files = null"
+                                class="mt-2 text-xs bg-white rounded-md shadow-md uppercase border
+                                    border-primary-100 cursor-pointer hover:bg-primary-100 hover:text-white text-primary-100
+                                    ease-linear transition-all py-1 px-2"
+                            >Salvar</button>
+                        </div>
+                    </form>
                 </div>
 
                 <h3 class="text-lg font-medium leading-6 text-gray-900">{{ $loggedUser['name'] }}</h3>
@@ -47,14 +62,16 @@
         <div class="mt-5 px-2">
             <div class="flex flex-row w-full">
                 <a href="{{ route('app.interest', 'edit') }}"
-                        class="flex flex-row space-x-2 justify-center items-center bg-primary-100 p-4 text-white w-full text-sm rounded-full mt-8 font-bold transform duration-150 active:scale-95"
+                        class="flex flex-row space-x-2 justify-center items-center bg-primary-100 p-4 text-white
+                        w-full text-sm rounded-full mt-8 font-bold transform duration-150 active:scale-95"
                 >
                     <span>Alterar interesses</span>
                 </a>
             </div>
             <div class="flex flex-row w-full">
                 <a href="{{ route('app.knowledge', 'edit') }}"
-                        class="flex flex-row space-x-2 justify-center items-center bg-primary-100 p-4 text-white w-full text-sm rounded-full mt-8 font-bold transform duration-150 active:scale-95"
+                        class="flex flex-row space-x-2 justify-center items-center bg-primary-100 p-4 text-white
+                        w-full text-sm rounded-full mt-8 font-bold transform duration-150 active:scale-95"
                 >
                     <span>Alterar habilidades</span>
                 </a>
