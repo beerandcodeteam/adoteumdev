edb350ebae11b1aafdfec13870d78b3aeab1ed7b
Kelver <kelver_kmv@hotmail.com>
2021-12-09 18:10:52 -0300

    Test: Correção nos testes.

--- tests/Feature/ProfileScreenTest.php
+++ tests/Feature/ProfileScreenTest.php
@@ -3,7 +3,7 @@
     use App\Http\Livewire\Components\ProfileScreen;
     use App\Models\Profile;
     use App\Models\User;
-    use Livewire\TemporaryUploadedFile;
+    use Illuminate\Http\UploadedFile;
     use function Pest\Laravel\get;
 
 it('checks if ProfileScreen required auth', function () {
@@ -21,17 +21,17 @@ it('checks if ProfileScreen Component was rendered with auth', function () {
 
 it('checks if ProfileScreen Component was upload avatar', function () {
     $user = User::firstWhere('email', '33piter@adoteum.dev');
-    Storage::fake('public');
-    $file = TemporaryUploadedFile::fake()->image("avatar.png");
+
+    $file = UploadedFile::fake()->image("avatar.png");
     $nameAvatar = "avatar{$user->id}.png";
 
     actingAs($user->load('profile'))
         ->livewire(ProfileScreen::class)
         ->set('imageProfile', $file)
         ->call('storeAvatar');
-
     $profile = Profile::where('user_id', $user->id)->first();
-    $this->assertEquals($profile->avatar, url(storage_path("avatarProfile/{$nameAvatar}")));
 
-    Storage::disk('public')->assertExists("avatarProfile/{$nameAvatar}");
+    $this->assertEquals($profile->avatar, url("storage/avatarProfile/{$nameAvatar}"));
+
+    Storage::assertExists("avatarProfile/{$nameAvatar}");
 });
